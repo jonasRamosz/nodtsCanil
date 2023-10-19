65023d62e30bd02752a308ee57f912beafa15a78
jonasRamosz <jrenovaveis@gmail.com>
2023-10-19 11:14:45 -0300

    commit final

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response} from "express";
 
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
+
 export const search = (req:Request, res:Response) =>{
-    //res.render('pages/search')
+    let querry:string = req.query.q as string;
+    
+    if(!querry) {
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(querry);     
+    
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        querry
+    })
 }
